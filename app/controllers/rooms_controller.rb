e1b7d4e2679620c80c14af52cc7851e40973f09d
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-08-15 03:18:13 +0900

    画像表示の実装2

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,10 +17,10 @@ class RoomsController < ApplicationController
  end
 
  def destroy
-  room = Room.find(params[:id])
-  room.destroy
-  redirect_to root_path
-end
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
 
  private
 
